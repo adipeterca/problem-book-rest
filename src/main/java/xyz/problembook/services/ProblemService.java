d5942fc014fe5d8b9eee55baaed3cb0c8275e60c
adipeterca <adipeterca@gmail.com>
2021-05-30 15:01:35 +0300

    Update ProblemService.java

--- src/main/java/xyz/problembook/services/ProblemService.java
+++ src/main/java/xyz/problembook/services/ProblemService.java
@@ -25,7 +25,8 @@ public class ProblemService {
      * @return the ID of the new added problem
      */
     public int add(ProblemDTO problemDTO) {
-        int id = (int)problemRepository.count() + 1;
+        int id = (int)problemRepository.count();
+        id = id + 1;
         problemRepository.save(new ProblemEntity(id, problemDTO.getTeacherId(), problemDTO.getTeacherEmail(), problemDTO.getHint1(), problemDTO.getHint2(), problemDTO.getContent()));
         return problemRepository.getIdOf(problemDTO.getTeacherId(), problemDTO.getHint1(), problemDTO.getHint2(), problemDTO.getContent()).getId();
         // return 0;
